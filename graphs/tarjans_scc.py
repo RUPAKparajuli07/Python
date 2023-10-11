1e0c363c8b462f4b768ca538539996bcccfe91bb
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-10-11 17:11:33 +0545

    Update tarjans_scc.py

--- graphs/tarjans_scc.py
+++ graphs/tarjans_scc.py
@@ -1,51 +1,40 @@
 from collections import deque
 
-
 def tarjan(g):
     """
-    Tarjan's algo for finding strongly connected components in a directed graph
+    Tarjan's algorithm for finding strongly connected components in a directed graph.
 
-    Uses two main attributes of each node to track reachability, the index of that node
-    within a component(index), and the lowest index reachable from that node(lowlink).
+    Args:
+    - g: The directed graph represented as an adjacency list.
 
-    We then perform a dfs of the each component making sure to update these parameters
-    for each node and saving the nodes we visit on the way.
-
-    If ever we find that the lowest reachable node from a current node is equal to the
-    index of the current node then it must be the root of a strongly connected
-    component and so we save it and it's equireachable vertices as a strongly
-    connected component.
-
-    Complexity: strong_connect() is called at most once for each node and has a
-    complexity of O(|E|) as it is DFS.
-    Therefore this has complexity O(|V| + |E|) for a graph G = (V, E)
+    Returns:
+    - A list of strongly connected components.
     """
-
-    n = len(g)
-    stack = deque()
-    on_stack = [False for _ in range(n)]
-    index_of = [-1 for _ in range(n)]
-    lowlink_of = index_of[:]
+    n = len(g)  # Number of vertices in the graph
+    stack = deque()  # Stack for DFS traversal
+    on_stack = [False for _ in range(n)]  # Tracks whether a node is on the stack
+    index_of = [-1 for _ in range(n)]  # Index of each node
+    lowlink_of = index_of[:]  # Lowest index reachable from each node
 
     def strong_connect(v, index, components):
-        index_of[v] = index  # the number when this node is seen
-        lowlink_of[v] = index  # lowest rank node reachable from here
+        index_of[v] = index  # Assign the current index to the node
+        lowlink_of[v] = index  # Initialize lowlink with the current index
         index += 1
         stack.append(v)
         on_stack[v] = True
 
         for w in g[v]:
             if index_of[w] == -1:
+                # Recursive call to explore a new node
                 index = strong_connect(w, index, components)
-                lowlink_of[v] = (
-                    lowlink_of[w] if lowlink_of[w] < lowlink_of[v] else lowlink_of[v]
-                )
+                # Update the lowlink for the current node
+                lowlink_of[v] = min(lowlink_of[v], lowlink_of[w])
             elif on_stack[w]:
-                lowlink_of[v] = (
-                    lowlink_of[w] if lowlink_of[w] < lowlink_of[v] else lowlink_of[v]
-                )
+                # Update lowlink if w is on the stack
+                lowlink_of[v] = min(lowlink_of[v], index_of[w])
 
         if lowlink_of[v] == index_of[v]:
+            # Found a strongly connected component
             component = []
             w = stack.pop()
             on_stack[w] = False
@@ -60,17 +49,26 @@ def tarjan(g):
     components = []
     for v in range(n):
         if index_of[v] == -1:
+            # Start DFS for a new component
             strong_connect(v, 0, components)
 
     return components
 
-
 def create_graph(n, edges):
+    """
+    Create a directed graph represented as an adjacency list.
+
+    Args:
+    - n: Number of vertices.
+    - edges: List of directed edges as tuples (source, target).
+
+    Returns:
+    - The adjacency list representation of the graph.
+    """
     g = [[] for _ in range(n)]
     for u, v in edges:
         g[u].append(v)
     return g
-
 
 if __name__ == "__main__":
     # Test
@@ -80,4 +78,5 @@ if __name__ == "__main__":
     edges = list(zip(source, target))
     g = create_graph(n_vertices, edges)
 
+    # Find strongly connected components and assert the result
     assert [[5], [6], [4], [3, 2, 1, 0]] == tarjan(g)
